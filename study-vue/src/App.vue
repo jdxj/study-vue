7d190dce9f9aaf1255faa705ea0069afa3c69f4c
wangyq <985759262@qq.com>
2023-10-09 02:29:42 +0800

    test

---
+++ study-vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <router-link to="/">首页</router-link> |
+    <router-link to="/about">关于</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
